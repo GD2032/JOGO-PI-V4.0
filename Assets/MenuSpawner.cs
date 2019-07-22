c3afd1eda1e243b642e405f6a9e1f95f8e6f6237
Gabriel Dias <gabrieldias2032@gmail.com>
2019-07-22 00:35:33 -0300

    ajustes na areia

---
+++ Assets/MenuSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject peixes;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("invokePeixes", 2f, 4f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void invokePeixes()
+    {
+        Destroy(Instantiate(peixes,new Vector2(0f,0f), Quaternion.identity), 4f);
+    }
+}
