8f5d991e7ba00a7639733d517f1e90b106243527
gabriel dias <gabrieldias2032@gmail.com>
2019-07-31 12:07:54 -0300

    Pause incompleto

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -1,12 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PauseMenu : MonoBehaviour
 {
-    private bool pause = false;
+    [SerializeField] private Image[] buttons;
+    private static bool pause = false;
     [SerializeField]
     private GameObject menuUI;
+    private float Input;
+    private bool right;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,10 +20,17 @@ public class PauseMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        Input = Input.GetAxis("Vertical");
+        if (Input.GetKeyDown(KeyCode.P) || Input.GetKeyDown(KeyCode.Joystick1Button7))
         {
-            Pausado();
+            Pause();
         }
+        if (Input != 0 && cooldown)
+        {
+            right = RightTest(Input);
+            TrocarBotoes(right);
+        }
+
     }
     public void Resume() {
 
@@ -27,9 +38,37 @@ public class PauseMenu : MonoBehaviour
         Time.timeScale = 1f;
         pause = false; 
     }
-    public void Pausado() {
+    public void Pause() {
         menuUI.SetActive(true);
         Time.timeScale = 0f;
         pause = true;
     }
+    private void TrocarBotao(bool right)
+    {
+        switch(botaoAtual.tag)
+        {
+            case "Resume":
+                botaoAtual.tag = TrocarBotoes(right ? 1 : 3);
+                goto case anim;
+            case "SoundOff":
+                botaoAtual.tag = TrocarBotoes(right ? 2 : 0);
+            case "SoundVolue":
+                botaoAtual.tag = TrocarBotoes(right ? 3 : 1);
+                break;
+            case "Menu":
+                botaoAtual.tag = TrocarBotoes(right ? 0 : 2);
+                break;
+            case anim:
+                TrocarSprite();
+        }
+    }
+    private void TrocarSprite()
+    {
+        switch(botaoAtual.tag)
+        {
+            
+        }
+    }
+    private void RightTest(float Input) => Input > 0;
+    private string TrocarBotoes(float casa) => buttons[casa].tag;
 }
